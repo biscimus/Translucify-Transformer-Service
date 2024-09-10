9ba3b4fa5193c470882a1301e876ee3d35882b76
geonhoyun <geonhoyun@gmail.com>
2024-09-10 17:22:59 +0200

    Move probs back to cpu

--- simple_transformer.py
+++ simple_transformer.py
@@ -218,8 +218,8 @@ def translucify_with_transformer(id: int, log: pd.DataFrame, threshold: float, d
             probabilities = torch.sigmoid(logits)  # Apply sigmoid to convert logits to probabilities
             print(probabilities)
 
-            # Convert probabilities to numpy array for further processing
-            probabilities = probabilities.numpy()
+            # Move probabilities to cpu and convert to numpy array for further processing
+            probabilities = probabilities.cpu().numpy()
             print("probs: ", probabilities)
             high_prob_indices = (probabilities > threshold).nonzero(as_tuple=True)[1]
             string_labels = le.inverse_transform(high_prob_indices)
